3b736e80e57a435db0da9762a817ef9d3fb35a77
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-07 17:06:11 +0530

    dowhile

--- track/M02/Nesteddowhile.java
+++ track/M02/Nesteddowhile.java
@@ -1,20 +1,21 @@
 
-class Nestedwhile {
+public class Nesteddowhile {
 
     public static void main(String[] args) {
 
         int i = 1;
 
-        while (i <= 5) {
+        do {
             int j = 1;
 
-            while (j <= 5) {
+            do {
                 System.out.println(j + " ");
                 j++;
-            }
+            } while (j <= 5);
 
             System.out.println();
             i++;
-        }
+
+        } while (i <= 5);
     }
 }
