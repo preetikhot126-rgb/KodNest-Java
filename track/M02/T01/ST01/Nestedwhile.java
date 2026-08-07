1d27d7652ce0d733053227c9f53bcfe7a911aef5
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-07 16:46:32 +0530

    while

---
+++ track/M02/T01/ST01/Nestedwhile.java
@@ -0,0 +1,20 @@
+
+class Nestedwhile {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            int j = 1;
+
+            while (j <= 5) {
+                System.out.print("* ");
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+}
