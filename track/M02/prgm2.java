92d5331c5c32a71d02f5b7801202df124a2d67f3
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-11 15:29:14 +0530

    pra no return

---
+++ track/M02/prgm2.java
@@ -0,0 +1,12 @@
+
+class prgm2 {
+
+    public static void main(String[] args) {
+        add(10, 5);
+    }
+
+    static void add(int a, int b) {
+        int res = a + b;
+        System.out.println(res);
+    }
+}
