2b5b0be9e83ea63ff3af81d53ecf71c5f3a02953
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-11 16:01:24 +0530

    no per return

---
+++ track/M02/prgm3.java
@@ -0,0 +1,17 @@
+class prgm3 {
+    public static void main(String[] args) {
+         intint res = add();
+         System.out.println(res);
+    }
+    {
+        int a = 10;
+        int
+    }static in()
+    t add()
+    {
+        int a = 10;
+        int b = 20;
+        int res =  a + b;
+        return res
+    }
+}
---
+++ track/M02/prgm4.java
@@ -0,0 +1,11 @@
+class prgm4 {
+    public static void main(String[] args) {
+        int res = add(10,20);
+        System.out.println(res);
+    }
+    static int add(int a, int b)
+    {
+        int res = a+b;
+        return res;
+    }
+}
