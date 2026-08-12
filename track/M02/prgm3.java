2950cf4c36dcbcb686e5e7bd853a80d2448a746d
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-12 15:43:11 +0530

    Shadowing

---
+++ track/M02/prgm3.java
@@ -0,0 +1,15 @@
+
+class prgm3 {
+
+    public static void main(String[] args) {
+        int res = add();
+        System.out.println(res);
+    }
+
+    static int add() {
+        int a = 10;
+        int b = 20;
+        int res = a + b;
+        return res;
+    }
+}
