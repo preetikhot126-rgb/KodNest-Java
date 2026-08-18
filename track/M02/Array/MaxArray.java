e3956cc7c60c867098246a7f703085105ee7a84d
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-18 15:14:10 +0530

    MaxArray

---
+++ track/M02/Array/MaxArray.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class MaxArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.println("Enter Array Elements : ");
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are : ");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int max = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum Elements is : " + max);
+    }
+}
