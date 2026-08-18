ef9a08efe07dc288030a7618ac840ec1743bad78
Preeti Balannavar <preetikhot126@gmail.com>
2026-08-18 15:59:38 +0530

    ArrayKey

--- track/M02/Array/MinArray.java
+++ track/M02/Array/MinArray.java
@@ -7,16 +7,16 @@ public class MinArray {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
         System.out.println("Enter Array Elements : ");
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Array Elements are : ");
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
         int min = arr[0];
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             if (arr[i] < min) {
                 min = arr[i];
             }
