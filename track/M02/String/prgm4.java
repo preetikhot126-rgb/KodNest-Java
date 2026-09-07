2fa6964a03235bb4e080ec2af116c37eb57759f4
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-07 19:51:25 +0530

    String4

---
+++ track/M02/String/prgm4.java
@@ -0,0 +1,13 @@
+
+public class prgm4 {
+
+    public static void main(String[] args) {
+        String s3 = new String("Java");
+        String s4 = new String("Java");
+        if (s3.equals(s4)) {
+            System.out.println("String are same");
+        } else {
+            System.out.println("String are not-equal");
+        }
+    }
+}
