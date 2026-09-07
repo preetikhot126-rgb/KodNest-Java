43b0b38fd66be7459cdcdaeb1119e8be93e8e62a
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-07 19:39:32 +0530

    String3

---
+++ track/M02/String/prgm3.java
@@ -0,0 +1,13 @@
+
+public class prgm3 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1.equals(s2)) {
+            System.out.println("String are same");
+        } else {
+            System.out.println("String are not-equal");
+        }
+    }
+}
