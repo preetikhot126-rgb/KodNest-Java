009f99320785913b4c24e5f26191c01a245357eb
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-08 20:15:56 +0530

    String Method3

---
+++ track/M02/String/StringMethod3.java
@@ -0,0 +1,9 @@
+
+public class StringMethod3 {
+
+    public static void main(String[] args) {
+        String s = "   Java   ";
+        System.out.println(s);
+        System.out.println(s.trim());
+    }
+}
