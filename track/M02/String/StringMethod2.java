b381374ffb181ae543c4fd6d650bb1e2a3c7221f
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-08 20:12:57 +0530

    String Method2

---
+++ track/M02/String/StringMethod2.java
@@ -0,0 +1,15 @@
+
+public class StringMethod2 {
+
+    public static void main(String[] args) {
+        String s1 = "Raja";
+        System.out.println(s1.isEmpty());
+        System.out.println(s1.isBlank());
+        String s2 = "";
+        System.out.println(s2.isEmpty());
+        System.out.println(s2.isBlank());
+        String s3 = " ";
+        System.out.println(s3.isEmpty());
+        System.out.println(s3.isBlank());
+    }
+}
