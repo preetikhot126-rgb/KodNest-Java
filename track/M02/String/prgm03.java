8720f34cdad87645b37f940efa153618422e02dc
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-09 19:25:29 +0530

    Stringchar

--- track/M02/String/prgm03.java
+++ track/M02/String/prgm03.java
@@ -5,7 +5,7 @@ public class prgm03 {
         String s1 = "Java";
         System.out.println(s1);
         char arr[] = s1.toCharArray();
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             System.out.println(arr[i]);
         }
         String res = new String(arr);
