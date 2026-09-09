f2df625a77f0ab19c9e8b6817799d9ab2ad57c43
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-09 19:12:41 +0530

    StringUpper

---
+++ track/M02/String/prgm01.java
@@ -0,0 +1,10 @@
+
+public class prgm01 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+        System.out.println(res);
+        System.out.println(s1);
+    }
+}
