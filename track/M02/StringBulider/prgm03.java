cdafb0f3beb4a167e96a423877cbd4d0b2285f56
Preeti Balannavar <preetikhot126@gmail.com>
2026-09-10 20:11:45 +0530

    StringBuilder03

---
+++ track/M02/StringBulider/prgm03.java
@@ -0,0 +1,18 @@
+public class prgm03 {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Java");
+        System.out.println(sb1);
+        
+        sb1.append("Program");
+        System.out.println(sb1);
+
+        sb1.insert(0, "Python");
+        System.out.println(sb1);
+
+        sb1.delete(0, 4);
+        System.out.println(sb1);
+
+        sb1.reverse();
+        System.out.println(sb1);
+    }
+}
